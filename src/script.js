df1eb79faebc63848b31010c84e6687d41abbe26
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-10-07 16:20:49 +0100

    EOC105 completed

--- src/script.js
+++ src/script.js
@@ -8,60 +8,75 @@ const gids = ["0", "1574569648", "1605451198"];
 // TODO your code here
 let employees = [];
 
-async function getEmployeesData() {
-  const nameResponse = await fetch(
-    `${dataURLBase}${id}${dataURLEnd}${gids[0]}`
-  );
-  const employeesName = await nameResponse.text();
-  const convertedEmployeesName = JSON.parse(
-    employeesName.substring(47).slice(0, -2)
-  ).table.rows;
+async function fetchData(link){
+  const response  = await fetch(link);
+  const dataText = await response.text();
+  const dataTextConvertedToJson = JSON.parse(dataText.slice(47, -2));
+  return dataTextConvertedToJson.table.rows
+}
 
-  const dateResponse = await fetch(
-    `${dataURLBase}${id}${dataURLEnd}${gids[1]}`
-  );
-  const employeesDate = await dateResponse.text();
-  const convertedEmployeesDate = JSON.parse(
-    employeesDate.substring(47).slice(0, -2)
-  ).table.rows;
+function createEmployeesTable(employeesData){
+  let table = document.getElementById("employees");
+  table.border = "1";
 
-  const salaryResponse = await fetch(
-    `${dataURLBase}${id}${dataURLEnd}${gids[2]}`
-  );
-  const employeesSalary = await salaryResponse.text();
-  const convertedEmployeesSalary = JSON.parse(
-    employeesSalary.substring(47).slice(0, -2)
-  ).table.rows;
+  const tableHeading = ["Last Name", "First Name", "Hire Date", "Salary"];
+  let trHeading = document.createElement("tr");
+  table.appendChild(trHeading);
+  for(let i = 0; i<tableHeading.length; i++){
+    let th = document.createElement("th");
+    th.innerHTML = tableHeading[i];
+    th.style.textAlign = "center";
+    trHeading.appendChild(th);
+  }
+  
+  for(let employee of employeesData){
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
 
-  for (let i = 1; i < convertedEmployeesName.length; i++) {
+    for(let property in employee){
+      let td = document.createElement("td");
+      td.innerHTML = (employee[property]);
+      tr.appendChild(td);
+    }
+  }
+}
+
+async function employeesData(){
+  let link = `${dataURLBase}${id}${dataURLEnd}${gids[0]}`;
+  let rows = await fetchData(link);
+  let i = 0;
+  for(let row of rows){
+    if(row.c[0].v == "first"){
+      continue;
+    }
     employees[i] = {
-      firstName: convertedEmployeesName.c[0].v,
-      lastName: convertedEmployeesName.c[1].v,
-    };
+      "last Name": row.c[1].v,
+      "first Name": row.c[0].v
+    }
+    i++;
   }
 
-  // console.log(convertedEmployeesName);
-}
-getEmployeesData();
+  link = `${dataURLBase}${id}${dataURLEnd}${gids[1]}`;
+  rows = await fetchData(link);
+  i = 0;
+  for(let row of rows){
+    employees[i] = {
+      ...employees[i], "hiredDate": (new Date(row.c[0].f)).toDateString().slice(4, 15)
+    };
+    i++;
+  }
 
-async function employeesData() {
-  const rowData = await getEmployeesData();
-  const dataArr = rowData.map((dataSheet) => {
-    return dataSheet.map((row) => {
-      return row.c.map((cell) => {
-        return cell.v;
-      });
-    });
-  });
-  return dataArr;
+  link = `${dataURLBase}${id}${dataURLEnd}${gids[2]}`;
+  rows = await fetchData(link);
+  i = 0;
+  for(let row of rows){
+    employees[i] = {
+      ...employees[i],
+      "salary": Intl.NumberFormat("en-US",{style: "currency", currency: "USD", maximumFractionDigits: 2}).format(row.c[0].v)
+    }
+    i++;
+  }
+
+  createEmployeesTable(employees);
 }
 employeesData();
-
-// console.log(employeesData());
-
-async function dataObj() {
-  const dataArray = await employeesData();
-  for (let i = 0; i < dataArray.length; i++) {
-    data.push(i);
-  }
-}
